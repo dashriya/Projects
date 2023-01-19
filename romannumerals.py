226738cc3c216abc052a0f535347f9b3362daa7b
dashriya <shriyadas2018@gmail.com>
2023-01-19 12:10:40 -0800

    roman numerals code

---
+++ romannumerals.py
@@ -0,0 +1,24 @@
+import random
+
+num = random.randint(1, 10)
+if num == 1:
+    print("Roman number equivalent to " + str(num) + " is I.")
+elif num == 2:
+    print("Roman number equivalent to " + str(num) + " is II.")
+elif num == 3:
+    print("Roman number equivalent to " + str(num) + " is III.")
+elif num == 4:
+    print("Roman number equivalent to " + str(num) + " is IV.")
+elif num == 5:
+    print("Roman number equivalent to " + str(num) + " is V.")
+elif num == 6:
+    print("Roman number equivalent to " + str(num) + " is VI.")
+elif num == 7:
+    print("Roman number equivalent to " + str(num) + " is VII.")
+elif num == 8:
+    print("Roman number equivalent to " + str(num) + " is VIII.")
+elif num == 9:
+    print("Roman number equivalent to " + str(num) + " is IX.")
+else:
+    if num== 10:
+        print("Roman number equivalent to " + str(num) + " is X.")
